add2c184dacb4c5067e3fdbd77c9163a137fe4ad
isa <143816039+isaxsccp@users.noreply.github.com>
2026-02-05 07:19:08 -0300

    Add files via upload

---
+++ 2-ano/DS/heranca-em-java/15-05/Carro.java
@@ -0,0 +1,16 @@
+public class Carro extends Veiculo {
+	private int quantidadePortas;
+	public int getQuantidadePortas() {
+		return quantidadePortas;
+	}
+	public void setQuantidadePortas(int quantidadePortas) {
+		this.quantidadePortas = quantidadePortas;
+	}
+
+	@Override 
+	void exibirDados() {
+		super.exibirDados();
+		System.out.println("A quantidade de portas é: "+this.quantidadePortas);
+	}
+	
+}
