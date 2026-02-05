694e483e7c06f40f187978a76647b3a399ee7e79
isa <143816039+isaxsccp@users.noreply.github.com>
2026-02-05 06:52:21 -0300

    Add files via upload

---
+++ 1-ano/TPA/20-10 - Dezafios/Ex2.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+public class Ex2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        //cade o codigo da tabuada sua burraaaaaaaaaaaaaaaaaaa
